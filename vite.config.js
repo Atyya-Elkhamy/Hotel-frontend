f4855eee18705c0f1595022eb17dc7f6ac7cbc4b
Atyya-Elkhamy <atyyaelkhamy50@gmail.com>
2025-11-27 00:26:05 +0200

    dddd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: true,
+    allowedHosts: ["hotels.rootmatrix.cloud"],  // 👈 IMPORTANT FIX
+  },
+
+  preview: {
+    host: true,
+    allowedHosts: ["hotels.rootmatrix.cloud"],  // 👈 IMPORTANT FIX
+  },
 })
